e88b612d2a699f47b548c43085abf31bd6faee10
gimenosg <gimenosg@amazon.com>
2024-06-01 10:40:07 +0100

    Account registration - error handling is missing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
-
+import * as bcrypt from 'bcrypt'; // Import the 'bcrypt' package
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -14,6 +14,8 @@ export class UserController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     console.log(createUserDto);
-    return this.userService.create(createUserDto);
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const newUser = { ...createUserDto, password: hashedPassword };
+    return this.userService.create(newUser);
   }
 }
